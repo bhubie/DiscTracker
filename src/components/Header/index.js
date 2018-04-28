6c89deb909cc1eb00ff78255e4ba1d0803b3fa81
Brett Huber <bretthuber6@gmail.com>
2018-04-28 10:46:40 -0500

    added icon and logic to open github repository

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Header as GlamHeader, H1 } from 'glamorous';
+import { Header as GlamHeader, H1, Nav } from 'glamorous';
 import AppTheme from '../../AppTheme';
+import IconGitHub from '../Icons/IconGitHub';
 
 const headerStyle = {
   left: 0,
@@ -12,21 +13,42 @@ const headerStyle = {
   gridArea: 'header',
   marginBottom: '24px',
   color: AppTheme.palette.alternateTextColor,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
 };
 
 const h1Style = {
-  float: 'left',
+  margin: '0',
   fontSize: '24px',
-  fontWeight: 400,
+  fontWeight: '400',
+};
+
+const styleIcon = {
+  color: AppTheme.palette.alternateTextColor,
+  fill: 'currentColor',
+};
+
+const styleNav = {
+  fontSize: '100%',
 };
 
 const Header = () => (
-  <GlamHeader css={headerStyle}>
+  <GlamHeader css={headerStyle} id="Header">
     <H1 css={h1Style}>
       Disc Tracker
     </H1>
+    <Nav id="headerNav" css={styleNav}>
+      <a
+        href="https://github.com/bhubie/DiscTracker"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Contribute on Github"
+      >
+        <IconGitHub id="githubIcon" style={styleIcon} />
+      </a>
+    </Nav>
   </GlamHeader>
 );
 
 export default Header;
-// export default muiThemeable()(Header);
