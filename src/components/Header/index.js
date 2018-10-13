fffaf16692fa46b8432670ca8d5472a8594cd8e2
Brett Huber <bretthuber6@gmail.com>
2018-10-13 12:47:53 -0500

    replace glam with bulma styles in header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,55 +1,37 @@
 import React from 'react';
-import { Header as GlamHeader, H1, Nav } from 'glamorous';
 import AppTheme from '../../AppTheme';
 import IconGitHub from '../Icons/IconGitHub';
-import Link from '../Link/index';
 
-const headerStyle = {
-  left: 0,
-  top: 0,
-  height: '56px',
-  padding: '0 24px',
-  background: AppTheme.palette.primary2Color,
-  zIndex: 50,
-  gridArea: 'header',
-  marginBottom: '24px',
-  color: AppTheme.palette.alternateTextColor,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-};
-
-const h1Style = {
-  margin: '0',
-  fontSize: '24px',
-  fontWeight: '400',
-};
 
 const styleIcon = {
   color: AppTheme.palette.alternateTextColor,
   fill: 'currentColor',
 };
 
-const styleNav = {
-  fontSize: '100%',
-};
-
 const Header = () => (
-  <GlamHeader css={headerStyle} id="Header">
-    <H1 css={h1Style}>
-      Disc Tracker
-    </H1>
-    <Nav id="headerNav" css={styleNav}>
-      <Link
-        href="https://github.com/bhubie/DiscTracker"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="Contribute on Github"
+  <nav className="navbar header" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <a
+        className="navbar-item subtitle is-4"
+        href="https://disctracker.netlify.com/"
       >
-        <IconGitHub id="githubIcon" style={styleIcon} />
-      </Link>
-    </Nav>
-  </GlamHeader>
+        Disc Tracker
+      </a>
+    </div>
+
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <a
+          href="https://github.com/bhubie/DiscTracker"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Contribute on Github"
+        >
+          <IconGitHub id="githubIcon" style={styleIcon} />
+        </a>
+      </div>
+    </div>
+  </nav>
 );
 
 export default Header;
