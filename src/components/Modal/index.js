14b0d5717f6d2600d216bb1b611ce2a068745ad8
Brett Huber <bretthuber6@gmail.com>
2018-12-30 11:33:42 -0600

    fix modal to return null if not opened

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 const Modal = ({
   title, open, onRequestClose, actions, children,
 }) => {
-  const modalCSSClass = open ? 'modal is-active' : 'modal';
-
-  return (
-    <div className={modalCSSClass}>
-      <div className="modal-background" />
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <p className="modal-card-title">{title}</p>
-          <button className="delete" aria-label="close" onClick={onRequestClose} />
-        </header>
-        <section className="modal-card-body">
-          {children}
-        </section>
-        <footer className="modal-card-foot">
-          {actions}
-        </footer>
+  if (open) {
+    return (
+      <div className="modal is-active">
+        <div className="modal-background" />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">{title}</p>
+            <button className="delete" aria-label="close" onClick={onRequestClose} />
+          </header>
+          <section className="modal-card-body">
+            {children}
+          </section>
+          <footer className="modal-card-foot">
+            {actions}
+          </footer>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return null;
 };
 
 Modal.propTypes = {
